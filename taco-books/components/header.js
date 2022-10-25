5796fa95cbcb6ae10c92d23d0ccc03b98ed4af46
clumsyNinja196 <67493116+clumsyNinja196@users.noreply.github.com>
2022-10-25 14:48:12 -0400

    updated responsiveness

--- taco-books/components/header.js
+++ taco-books/components/header.js
@@ -1,11 +1,12 @@
-import { Divider, Typography, Box } from '@mui/material';
+import { Divider, Typography, Box, Button } from '@mui/material';
 
 export default function Header() {
     return ( 
-        <Box className='header-bar' sx={{ backgroundColor: 'primary.main', float: 'left', width: '150px', height: '100%', p: '10px', borderRight: '1px solid white'}}>
+        <Box className='header-bar' sx={{ backgroundColor: 'primary.main', float: {xs: 'none', md: 'left'}, width: {xs: '100%', md: '200px'}, height:{xs: '165px', md: '100%'}, p: '10px', borderRight: '1px solid white'}}>
             <Typography variant="h3" color="white">Taco Books</Typography>
             <Divider color="white"/>
             <Typography variant="h6" color="white">A <a>Trello</a>-<a>Google Books</a> hybrid</Typography>
+            <Button sx={{color: 'secondary1.main', backgroundColor: 'white', mt: 2, }}>Search Books</Button>
         </Box>
     )
 }
--- taco-books/pages/index.js
+++ taco-books/pages/index.js
@@ -43,41 +43,37 @@ export default class extends React.Component {
       <ThemeProvider theme={appTheme}>
         <Box sx={{height: '100vh', backgroundColor: 'background.main'}}>
           <Header/>
-          <FormControl sx={{ml: '20px', mt: '20px', width: '200px', color: 'white'}}>
-            <NativeSelect
-              sx={{ backgroundColor: 'white', p: 1}}
-              defaultValue={10}
-              label='shelf'
-              inputProps={{
-                name: 'shelf',
-                id: 'uncontrolled-native',
-              }}
-            >
-              <option value={10}>Ten</option>
-              <option value={20}>Twenty</option>
-              <option value={30}>Thirty</option>
-            </NativeSelect>
-          </FormControl>
-          
-          <div className="horizontal-scroll">
-            {this.state.lists.map((list) =>
-              <BookList key={list.id}
-                        deleteList={()=>this.deleteCurrent(list.id)}/>
-            )}
-            <Button sx={{ml: '20px'}} variant="contained" color="secondary2" onClick={this.addList}><ControlPointIcon/></Button>
-          </div>
+          <Box sx={{display: 'flex', flexFlow: 'column', height: {xs: 'calc(100% - 165px)', md: '100%'}, py: 2}}>
+            <FormControl sx={{ml: '20px', mt: '20px', width: '200px', color: 'white'}}>
+              <NativeSelect
+                sx={{ backgroundColor: 'white', p: 1}}
+                defaultValue={10}
+                label='shelf'
+                inputProps={{
+                  name: 'shelf',
+                  id: 'uncontrolled-native',
+                }}
+              >
+                <option value={10}>Ten</option>
+                <option value={20}>Twenty</option>
+                <option value={30}>Thirty</option>
+              </NativeSelect>
+            </FormControl>
+            
+
+            <Box sx={{pt: '20px', overflowX: 'auto', maxWidth: '100vw', display: 'flex', flex: '1'}}>
+              {this.state.lists.map((list) =>
+                <BookList key={list.id}
+                          deleteList={()=>this.deleteCurrent(list.id)}/>
+              )}
+              <Button sx={{ml: '20px'}} variant="contained" color="secondary2" onClick={this.addList}><ControlPointIcon/></Button>
+            </Box>
+          </Box>
         </Box>
 
         <style jsx>{`
               .parent {
                 height: 100vh
-              }
-              .horizontal-scroll {
-                padding-top: 20px;
-                overflow-x: auto;
-                display: flex;
-                max-width: 100vw;
-                height: 88%;
               }
           `}</style>
       </ThemeProvider>
